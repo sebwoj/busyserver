3d8096dad63378144b12bc736a6516f0bf7d2965
Sebastian Wojciechowski <42519683+sebwoj@users.noreply.github.com>
2021-05-23 16:19:29 +0200

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo dnf install python3 python3-pip uwsgi uwsgi-router-http uwsgi-plugin-python3 -y
+sudo yum install python3 python3-pip uwsgi uwsgi-router-http uwsgi-plugin-python3 -y
 sudo pip install django
 
 uwsgi --ini deploy/uwsgi.ini
